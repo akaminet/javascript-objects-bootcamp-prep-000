8ee52a8dd3143c4a90aa7912fdd9291130231c1e
akaminet@umich.edu <akaminet@umich.edu>
2018-06-13 13:08:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = {beyonce: "Halo"}
 
 function updatePlaylist(obj, artist, song){
@@ -7,4 +8,15 @@ function updatePlaylist(obj, artist, song){
 
 function removeFromPlaylist(obj, artist){
   delete obj[artist]
+=======
+var playlist = {}
+
+function updatePlaylist(obj, artist, song){
+  Object.assign(obj, artist, song)
+  return obj
+}
+
+function removePlaylist(obj, artist){
+  delete obj.artist
+>>>>>>> 52da8996a0cbf49c236c5f2eadafefea8fe3d34a
 }
